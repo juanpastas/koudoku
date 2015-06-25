1d4c4f31f1948d6a5165c0f1778d9a14f752305f
Juan David Pastas <pastasjuan@gmail.com>
2015-06-25 15:23:34 -0500

    Change card to source when creating customer.

--- app/concerns/koudoku/subscription.rb
+++ app/concerns/koudoku/subscription.rb
@@ -70,7 +70,7 @@ module Koudoku::Subscription
               customer_attributes = {
                 description: subscription_owner_description,
                 email: subscription_owner_email,
-                card: credit_card_token # obtained with Stripe.js
+                source: credit_card_token # obtained with Stripe.js
               }
 
               # If the class we're being included in supports coupons ..
@@ -123,7 +123,7 @@ module Koudoku::Subscription
 
         # fetch the customer.
         customer = Stripe::Customer.retrieve(self.stripe_id)
-        customer.card = self.credit_card_token
+        customer.source = self.credit_card_token
         customer.save
 
         # update the last four based on this new card.
